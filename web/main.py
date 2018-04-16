cad7892b29c14b0440998ecc3497c261759e0dac
Maria Romero <m.romeroangulo@gmail.com>
2018-04-16 14:56:37 +0200

    add new stuff

--- web/main.py
+++ web/main.py
@@ -1,9 +1,17 @@
 from bottle import *
 from lxml import etree
+import sesion
 import requests
 from sys import argv
 import os
 
+session_opts = {
+    'session.type': 'memory',
+    'session.cookie_expires': 300,
+    #'session.data_dir': './data',
+    'session.auto': True
+}
+app = SessionMiddleware(app(), session_opts)
 
 @route('/')
 def inicio():
@@ -13,4 +21,5 @@ def inicio():
 def server_static(filepath):
     return static_file(filepath, root='static')  
 
-run(host='172.16.103.45',port=8080, debug=True)
+run(app=app,host='172.16.103.45', port=8080,reloader=True)
+
